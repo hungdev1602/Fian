c244a00fbdc2be7b7310cc18f4d10db42cdf92d7
Nguyen Dinh Hung <matrix16hung@gmail.com>
2025-01-12 00:59:10 +0300

    fix sliderAward.jsx

--- src/pages/AboutPage/Sections/Section3/SliderAward/SliderAward.jsx
+++ src/pages/AboutPage/Sections/Section3/SliderAward/SliderAward.jsx
@@ -24,53 +24,55 @@ const SliderAward = () => {
 
   return (
     <>
-      <Splide
-        options={{
-          arrows: false, // Hide navigation arrows
-          pagination: false, // Hide pagination dots
-          fixedWidth: '110px', // Fixed width for each slide
-          gap: '0px', // Gap between slides
-        }}
-      >
-        <SplideSlide>
-          <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award1)}>
-            <img src={award1} alt="" className="w-full h-full object-cover"/>
+      <div className="block sm:hidden">
+        <Splide
+          options={{
+            arrows: false, // Hide navigation arrows
+            pagination: false, // Hide pagination dots
+            fixedWidth: '110px', // Fixed width for each slide
+            gap: '0px', // Gap between slides
+          }}
+        >
+          <SplideSlide>
+            <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award1)}>
+              <img src={award1} alt="" className="w-full h-full object-cover"/>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award2)}>
+              <img src={award2} alt="" className="w-full h-full object-cover"/>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award3)}>
+              <img src={award3} alt="" className="w-full h-full object-cover"/>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award4)}>
+              <img src={award4} alt="" className="w-full h-full object-cover"/>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award5)}>
+              <img src={award5} alt="" className="w-full h-full object-cover"/>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award6)}>
+              <img src={award6} alt="" className="w-full h-full object-cover"/>
+            </div>
+          </SplideSlide>
+  
+        </Splide>
+        {isOpen && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-[999]" onClick={handleClose}>
+            <div className="max-w-[80%] h-[50vh] bg-white rounded-[20px] p-[20px]">
+              <img src={selectedImage} alt="" className="w-full h-full object-contain"/>
+            </div>
           </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award2)}>
-            <img src={award2} alt="" className="w-full h-full object-cover"/>
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award3)}>
-            <img src={award3} alt="" className="w-full h-full object-cover"/>
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award4)}>
-            <img src={award4} alt="" className="w-full h-full object-cover"/>
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award5)}>
-            <img src={award5} alt="" className="w-full h-full object-cover"/>
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="w-[100px] h-[110px] rounded-[20px] border border-[#AAA09A] p-[2px] truncate" onClick={() => handleClick(award6)}>
-            <img src={award6} alt="" className="w-full h-full object-cover"/>
-          </div>
-        </SplideSlide>
-
-      </Splide>
-      {isOpen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-[999]" onClick={handleClose}>
-          <div className="max-w-[80%] h-[50vh] bg-white rounded-[20px] p-[20px]">
-            <img src={selectedImage} alt="" className="w-full h-full object-contain"/>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   )
 }
